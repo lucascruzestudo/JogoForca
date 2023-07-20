c9ff3145b6ad284466fbd4999aea6be5b1ff1a16
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-20 09:42:26 -0300

    Vencer e perder implementado

--- forca.py
+++ forca.py
@@ -4,20 +4,33 @@ def jogar():
     print("************************************")
 
     palavra_secreta = "bolacha"
-    acerto = False
+    venceu = False
     enforcou = False
+    erros = 0
     letras_acertadas = ["_","_","_","_","_","_","_"]
 
-    while(not acerto and not enforcou):
-        print(letras_acertadas)
+    while(not venceu and not enforcou):
+        print(f"\n{letras_acertadas}")
 
-        chute = input("Digite uma letra do alfabeto: ").lower().strip()
+        chute = input("Digite uma letra do alfabeto: ").strip().lower()
 
-        for index, letra in enumerate(palavra_secreta):
-            if chute == letra:
-                letras_acertadas[index] = chute
+        if chute in palavra_secreta:
+            for index, letra in enumerate(palavra_secreta):
+                if chute == letra:
+                    letras_acertadas[index] = chute
+        else:
+            print("Letra não existe na palavra secreta!")
+            erros += 1
 
+        venceu = "_" not in letras_acertadas
+        
+        enforcou = erros == 6
 
+    if(venceu):
+        print("Você ganhou!")
+    else:
+        print("Acabaram suas tentativas!")
+        
     print("Fim de jogo!")
 
 if __name__ == "__main__":
